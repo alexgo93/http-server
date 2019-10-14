20b7e302b54ac3fc5ba5ce5c9df1550524cd35ed
alexgo93 <alekseimell5644@gmail.com>
2019-10-14 15:40:23 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# PHP Http Server
+[![Maintainability](https://api.codeclimate.com/v1/badges/592d397724b83a266cdd/maintainability)](https://codeclimate.com/github/alexgo93/http-server/maintainability)  [![Build Status](https://travis-ci.org/alexgo93/http-server.svg?branch=master)](https://travis-ci.org/alexgo93/http-server)
+
+
+# Installation
+
+composer require alexgo93/http-server
+
+# Description
+
+Simple http-server
+
+# Usage
+run ./bin/server-up
+after doing get-request on 'localhost' you' ll see the index.html from "/bin" directory
--- src/server.php
+++ src/server.php
@@ -24,15 +24,9 @@ class Server
 
         while ($client = socket_accept($sock)) {
             $userString = socket_read($client, 1024);
-
             $incoming = explode("\r\n", $userString);
             $fetchArray = explode(" ", $incoming[0]);
-
             $file = $fetchArray[1];
-            print_r($fetchArray);
-            echo "\n";
-            echo $file . "\n";
-
             if ($file == "/") {
                 $file = "index.html";
             } else {
@@ -41,17 +35,11 @@ class Server
                 $file = $fileArray[1];
             }
 
-            echo $file . "\n";
-
-            $output = '';
             $Header = "HTTP/1.1 200 OK \r\n" .
                 "Date: Fri, 31 Dec 1999 23:59:59 GMT \r\n" .
                 "Content-Type: text/html \r\n\r\n";
-
             $content = file_get_contents($file);
             $output = $Header . $content;
-            print_r($output);
-
             socket_write($client, $output, strlen($output));
             socket_close($client);
             socket_close($sock);
